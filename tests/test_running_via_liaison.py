6961b1034e3e355cdebb14fa68987f1e0224900f
TEParsons <todd.e.parsons@gmail.com>
2023-11-30 19:16:17 +0000

    TEST: Add local-only test for BBTK photodiode via Liaison

---
+++ tests/test_running_via_liaison.py
@@ -0,0 +1,23 @@
+from psychopy.tests.test_liaison import TestLiaison, runInLiaison
+
+
+class TestBBTKLiaison(TestLiaison):
+    def test_tpad_photodiode(self):
+        """
+        Test that a TPadPhotodiodeGroup can be set up and calibrated via Liaison
+        """
+        runInLiaison(
+            self.server, self.protocol, "DeviceManager", "addDevice",
+            "psychopy_bbtk.tpad.TPadPhotodiodeGroup", "diode",
+            "COM6", "2"
+        )
+        runInLiaison(
+            self.server, self.protocol, "DeviceManager", "callDeviceMethod",
+            "diode", "findThreshold",
+            "session.win", "1"
+        )
+        runInLiaison(
+            self.server, self.protocol, "DeviceManager", "callDeviceMethod",
+            "diode", "findPhotodiode",
+            "session.win", "1"
+        )
